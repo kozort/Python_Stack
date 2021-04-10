d8c12be9ccfe733a4d95335b59d0d9e64d9c2df9
kozort <timkozorezov@gmail.com>
2021-04-10 11:48:50 -0700

    completed functions)intermediate_2.py

---
+++ _python/python_fundamentals/functions_intermediate_2.py
@@ -0,0 +1,45 @@
+# # 1. Update Values in Dictionaries and Lists
+# x = [ [5,2,3], [10,8,9] ] 
+# students = [
+#      {'first_name':  'Michael', 'last_name' : 'Jordan'},
+#      {'first_name' : 'John', 'last_name' : 'Rosales'}
+# ]
+# sports_directory = {
+#     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+#     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+# }
+# z = [ {'x': 10, 'y': 20} ]
+
+# # Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
+# x[1][0] = 15
+# print(x)
+# # Change the last_name of the first student from 'Jordan' to 'Bryant'
+# students[0]['last_name'] = 'Bryant'
+# print(students)
+# # In the sports_directory, change 'Messi' to 'Andres'
+# sports_directory['soccer'][0] = 'Andres'
+# print(sports_directory)
+# # Change the value 20 in z to 30
+# z[0]['y'] = 30
+# print(z)
+
+# 2. Iterate Through a List of Dictionaries
+# Create a function iterateDictionary(some_list) that, given a list of dictionaries, the function loops through each dictionary in the list and prints each key and the associated value. For example, given the following list:
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+# iterateDictionary(students) 
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+def iterateDictionary(list):
+    for i in range (len(list)):
+        print(f"first_name - {list[i]['first_name']}, last_name - {list[i]['last_name']}")
+iterateDictionary(students)
