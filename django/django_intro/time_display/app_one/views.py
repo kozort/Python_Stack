59c88c811e0efcd5398d4a1b8820fc110cd75461
kozort <timkozorezov@gmail.com>
2021-05-17 20:00:28 -0700

    setup app_one complete

--- django/django_intro/time_display/app_one/views.py
+++ django/django_intro/time_display/app_one/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+def index(request):
+    return HttpResponse("home page")
 
 # Create your views here.
+
