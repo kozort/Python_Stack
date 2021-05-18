f69ef7238de4933a6af5631174eedf77e3dd0c6f
kozort <timkozorezov@gmail.com>
2021-05-17 19:37:08 -0700

    created django intro v env and project

---
+++ my_environments/git_Django/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/tim/Documents/GitHub/Python_Stack/my_environments/git_Django/bin/python3.9
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
