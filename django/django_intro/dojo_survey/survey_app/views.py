4a33d3833ff91d5009ef003b5925e8988e87cb6b
kozort <timkozorezov@gmail.com>
2021-05-22 00:07:03 -0700

    completed dojo_survey project

--- django/django_intro/dojo_survey/survey_app/views.py
+++ django/django_intro/dojo_survey/survey_app/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render, redirect
 
 def index(request):
-    context = {}
     return render(request, "index.html")
 
 def result(request):
-    context = {}
+    return render(request, "result.html")
+    
+def takeData(request):
+    if request.method == 'POST':
+        context = {
+            'yourName': request.POST['yourName'],
+            'favLanguage': request.POST['favLanguage'],
+            'location': request.POST['location'],
+            'comment':  request.POST['comment']
+        }
+        return render(request, "result.html", context)
     return render(request, "result.html")
