1de8d4e51b44df32d09d42a33d84c688c2c4bcc4
kozort <timkozorezov@gmail.com>
2021-05-23 16:13:21 -0700

    created dojos_ninjas project (in shell one-to-many

--- django/django_intro/ninja_gold/gold_app/urls.py
+++ django/django_intro/ninja_gold/gold_app/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('process', views.process)
+    path('farm', views.farm),
+    path('cave', views.cave),
+    path('house', views.house),
+    path('casino', views.casino),
+    path('clear', views.clear)
 ]
--- django/django_intro/ninja_gold/gold_app/views.py
+++ django/django_intro/ninja_gold/gold_app/views.py
@@ -1,7 +1,43 @@
 from django.shortcuts import render, redirect
+from time import gmtime, strftime
+import random
+
 
 def index(request):
+    if 'gold' not in request.session:
+        request.session['gold'] = 0
+        request.session['activities'] = []
+        print("created gold = 0")
     return render(request, 'index.html')
 
-def process(request):
+def farm(request):
+    if request.method == 'POST':
+        addGold(request,10,20)
     return redirect('/')
+
+def cave(request):
+    if request.method == 'POST':
+        addGold(request,5,10)
+    return redirect('/')
+
+def house(request):
+    if request.method == 'POST':
+        addGold(request,2,5)
+    return redirect('/')
+
+def casino(request):
+    if request.method == 'POST':
+        addGold(request,-50,50)
+    return redirect('/')
+
+def clear(request):
+    request.session['gold'] = 0
+    request.session['activities'] = []
+    return redirect("/")
+
+def addGold(request,min,max):
+    gold = random.randint(min,max)
+    request.session['gold'] += gold
+    date = strftime("%Y-%m-%d %H:%M %p", gmtime())
+    request.session['activities'].append(f"Earned {gold} from the farm! ({date})")
+    return request
