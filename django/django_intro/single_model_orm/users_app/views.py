f82b1bd1e593835b55935df9e9703fb397adf4c3
kozort <timkozorezov@gmail.com>
2021-07-14 16:28:54 -0700

    completed Users with Templates assignment

--- django/django_intro/single_model_orm/users_app/views.py
+++ django/django_intro/single_model_orm/users_app/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import USER
 
 def index(request):
-    return render(request, 'index.html')
+    context = {"USERS": USER.objects.all()}
+    return render(request, 'index.html', context)
+
+def takeUser(request):
+    if request.method == 'POST':
+        if request.POST['firstname'] != "":
+            if request.POST['lastname'] != "":
+                if request.POST['email'] != "":
+                    if request.POST['age'] != "":
+                        USER.objects.create(
+                            first_name = request.POST['firstname'], 
+                            last_name = request.POST['lastname'], 
+                            email_address = request.POST['email'],
+                            age = request.POST['age']
+                            )
+    return redirect('/')
