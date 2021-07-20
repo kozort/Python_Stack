c17cd7b5ad3c307ac472a26db591288c97a69b66
kozort <timkozorezov@gmail.com>
2021-07-19 23:23:49 -0700

    created all html pages, clickable prototype, everything seems to work except for 'update'

--- django/django_orm/tv_shows_proj/tv_shows_app/urls.py
+++ django/django_orm/tv_shows_proj/tv_shows_app/urls.py
@@ -1,4 +1,13 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('', views.index),]
+urlpatterns = [
+    path('', views.index),
+    path('shows', views.all_shows),
+    path('shows/new', views.shows_new),
+    path('shows/create', views.create),
+    path('shows/<int:showID>/edit', views.edit),
+    path('shows/<int:showID>/update', views.update),
+    path('shows/<int:showID>', views.show_detail),
+    path('shows/<int:showID>/destroy', views.delete),
+    ]
--- django/django_orm/tv_shows_proj/tv_shows_app/views.py
+++ django/django_orm/tv_shows_proj/tv_shows_app/views.py
@@ -1,6 +1,53 @@
 from django.shortcuts import render, redirect
+from .models import SHOW
 
 # Create your views here.
 def index(request):
-    #context = {"USERS": USER.objects.all()}
-    return render(request, 'index.html')
+    return redirect('/shows')
+
+def all_shows(request):
+    context = {"Shows": SHOW.objects.all()}
+    return render(request, 'all_shows.html', context)    
+
+def shows_new(request):
+    if request.method == 'GET':
+        return render(request, 'show_new.html')
+    return redirect('/shows')
+
+def create(request):
+    if request.method == 'POST':
+        SHOW.objects.create(
+            title = request.POST['title'],
+            network = request.POST['network'],
+            description = request.POST['description'],
+            release_date = request.POST['release_date']
+        )
+    return redirect(f'/shows/{SHOW.objects.last().id}')
+
+def show_detail(request, showID):
+    context = {"Show": SHOW.objects.get(id=showID)}
+    return render(request, 'show_detail.html', context)
+
+def edit(request, showID):
+    if request.method == 'GET':
+        context = {"Show": SHOW.objects.get(id=showID)}
+        return render(request, 'show_edit.html', context)
+    return redirect('/shows')
+
+def update(request, showID):
+    if request.method == 'POST':
+        c = SHOW.objects.get(id=showID)
+        c.title = request.POST['title']
+        c.network = request.POST['network']
+        c.description = request.POST['description']
+        c.release_date = request.POST['release_date']
+        # won't update for some reason, T/S this
+
+        return redirect(f'/shows/{showID}')
+    return redirect('/shows')
+
+def delete(request, showID):
+    if request.method == 'POST':
+        c = SHOW.objects.get(id=showID)
+        c.delete()
+    return redirect('/shows')
